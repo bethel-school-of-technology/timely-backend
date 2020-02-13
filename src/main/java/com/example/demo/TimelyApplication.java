ef693775557140ddb2c6375a92fb0fd9d09009fb
Rebekah <rjoychin@gmail.com>
2020-02-13 13:07:26 +0530

    created Java Spring project

---
+++ src/main/java/com/example/demo/TimelyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimelyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimelyApplication.class, args);
+	}
+
+}

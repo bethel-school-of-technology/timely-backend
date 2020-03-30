9b06a6f9c54d70c38c180f63e7bae544cb629b34
Pi0nStEaM <53586460+Pi0nStEaM@users.noreply.github.com>
2020-03-30 03:45:30 -0400

    Added & updated new tables to database such as 'Roles' & 'Users' & a joined 'User_Roles'

--- gnp/src/main/java/com/gnp/auth/User.java
+++ gnp/src/main/java/com/gnp/auth/User.java
@@ -1,25 +1,45 @@
 package com.gnp.auth;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
+@Table(name = "user", uniqueConstraints = { @UniqueConstraint(columnNames = "username"),
+		@UniqueConstraint(columnNames = "email") })
+
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
+	@NotBlank
+	@Size(max = 20)
 	@Column(nullable = false, unique = true)
 	private String username;
-	
+
 	private String firstName;
 
 	private String lastName;
 
 	private String company;
 
+	@NotBlank
+	@Size(max = 60)
+	@Email
 	private String email;
 
+	@NotBlank
+	@Size(max = 120)
 	private String password;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 
 	public Long getId() {
 		return id;
@@ -28,11 +48,11 @@ public class User {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
@@ -44,11 +64,11 @@ public class User {
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	
+
 	public String getLastName() {
 		return lastName;
 	}
-	
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}

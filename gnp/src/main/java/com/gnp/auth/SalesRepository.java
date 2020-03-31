a42dc3601c407c2d89d643f404f6df08eaa80566
Pi0nStEaM <53586460+Pi0nStEaM@users.noreply.github.com>
2020-03-31 02:14:15 -0400

    Backend.

---
+++ gnp/src/main/java/com/gnp/auth/SalesRepository.java
@@ -0,0 +1,9 @@
+package com.gnp.auth;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SalesRepository extends JpaRepository<Sales, Integer> {
+	List<Sales> findFirst28ByOrderByDateDesc();
+}

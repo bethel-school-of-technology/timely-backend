f8c212132bb5011401d1c0a53311b2ba8224e600
Rebekah <rjoychin@gmail.com>
2020-04-01 18:08:23 +0530

    made sales and estimate api routes accessible'

--- gnp/src/main/java/com/gnp/auth/SalesController.java
+++ gnp/src/main/java/com/gnp/auth/SalesController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/test")
 public class SalesController {
 	List<Sales> recentSales = new ArrayList<>();
 	List<Double> estimatedSales = new ArrayList<>();

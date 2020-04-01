2395bff70825fd23f29137e04b24b2eeb1897844
Rebekah <rjoychin@gmail.com>
2020-04-01 18:17:54 +0530

    changed api route for sales

--- gnp/src/main/java/com/gnp/auth/SalesController.java
+++ gnp/src/main/java/com/gnp/auth/SalesController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/test")
 public class SalesController {
 	List<Sales> recentSales = new ArrayList<>();
 	List<Double> estimatedSales = new ArrayList<>();

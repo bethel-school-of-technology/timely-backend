e65f17aadb9500d024383ea389c01241a1776638
Rebekah <rjoychin@gmail.com>
2020-04-09 11:03:45 +0530

    made date a unique column in database, added a SQL query to check if dates on sales post requests are unique or not, changed sales post method to return a message stating if the sales posts were successfully entered or duplicate entries

--- gnp/src/main/java/com/gnp/auth/repository/SalesRepository.java
+++ gnp/src/main/java/com/gnp/auth/repository/SalesRepository.java
@@ -1,11 +1,14 @@
 package com.gnp.auth.repository;
 
+import java.sql.Date;
 import java.util.List;
-
+import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gnp.auth.models.Sales;
 
+@Repository
 public interface SalesRepository extends JpaRepository<Sales, Integer> {
 	List<Sales> findFirst28ByOrderByDateDesc();
+	Boolean existsByDate(Date date);
 }
